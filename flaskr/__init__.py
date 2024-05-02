da103a679ce24a95ca2fc4870ce6fb361a7cd01d
Boluwatife <tifeasypeasy@gmail.com>
2024-05-02 15:43:30 +0100

    update db config to render db

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -6,8 +6,8 @@ from flask_login import LoginManager
 from flask_mail import Mail
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'be16ffeeed186879873c70376509e506'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db' 
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL') 
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
